83e1152c0cfa99a488554cf69edd5a36dad98ea4
Lukas Elmer <lukas.elmer@gmail.com>
2023-12-03 12:46:22 +0100

    Scripted auto-commit on change (2023-12-03 12:46:22) by gitwatch.sh

--- src/ledClock/ledClock.ts
+++ src/ledClock/ledClock.ts
@@ -2,72 +2,9 @@ import { consoleLog } from '../logger'
 import { truthy } from '../utils/array'
 
 export function ledClock(value: number) {
-  if (value === 0) {
-    consoleLog('')
-    singleDigit(0).forEach((line) => consoleLog(line))
-    consoleLog('')
-  }
-  if (value === 1) {
-    consoleLog('')
-    singleDigit(1).forEach((line) => consoleLog(line))
-    consoleLog('')
-  }
-  if (value === 2) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╔══╝')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
-  if (value === 3) {
-    consoleLog('')
-    consoleLog(' ══╗')
-    consoleLog('  ═╣')
-    consoleLog(' ══╝')
-    consoleLog('')
-  }
-  if (value === 4) {
-    consoleLog('')
-    consoleLog('╔  ╗')
-    consoleLog('╚══╣')
-    consoleLog('   ╝')
-    consoleLog('')
-  }
-  if (value === 5) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╚══╗')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
-  if (value === 6) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╠══╗')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
-  if (value === 7) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('   ║')
-    consoleLog('   ╝')
-    consoleLog('')
-  }
-  if (value === 8) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╠══╣')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
-  if (value === 9) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╚══╣')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
+  consoleLog('')
+  singleDigit(value).forEach((line) => consoleLog(line))
+  consoleLog('')
 }
 
 function singleDigit(num: number) {
