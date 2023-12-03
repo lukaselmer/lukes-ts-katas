c8f53b20a1550a2b30024554dc3e176575fdb031
Lukas Elmer <lukas.elmer@gmail.com>
2023-12-03 12:46:58 +0100

    Scripted auto-commit on change (2023-12-03 12:46:58) by gitwatch.sh

--- src/ledClock/ledClock.ts
+++ src/ledClock/ledClock.ts
@@ -1,58 +1,8 @@
 import { consoleLog } from '../logger'
-import { truthy } from '../utils/array'
+import { singleDigit } from './singleDigit'
 
 export function ledClock(value: number) {
   consoleLog('')
   singleDigit(value).forEach((line) => consoleLog(line))
   consoleLog('')
 }
-
-function singleDigit(num: number) {
-  if (num < 0 || num > 9) throw new Error(`Invalid digit: ${num}`)
-  return splitDigits()[num]
-}
-
-function splitDigits() {
-  const lines = rawDigits.split('\n').filter(truthy)
-  const height = lines.length / 10
-  const singleDigits: string[][] = []
-  lines.forEach((line, index) => {
-    const digitIndex = Math.floor(index / height)
-    singleDigits[digitIndex] = singleDigits[digitIndex] || []
-    singleDigits[digitIndex].push(line)
-  })
-  return singleDigits
-}
-
-const rawDigits = `
-╔══╗
-║  ║
-╚══╝
-   ╗
-   ║
-   ╝
-╔══╗
-╔══╝
-╚══╝
- ══╗
-  ═╣
- ══╝
-╔  ╗
-╚══╣
-   ╝
-╔══╗
-╚══╗
-╚══╝
-╔══╗
-╠══╗
-╚══╝
-╔══╗
-   ║
-   ╝
-╔══╗
-╠══╣
-╚══╝
-╔══╗
-╚══╣
-╚══╝
-`
---
+++ src/ledClock/singleDigit.ts
@@ -0,0 +1,49 @@
+import { truthy } from '../utils/array'
+
+export function singleDigit(num: number) {
+  if (num < 0 || num > 9) throw new Error(`Invalid digit: ${num}`)
+  return splitDigits()[num]
+}
+function splitDigits() {
+  const lines = rawDigits.split('\n').filter(truthy)
+  const height = lines.length / 10
+  const singleDigits: string[][] = []
+  lines.forEach((line, index) => {
+    const digitIndex = Math.floor(index / height)
+    singleDigits[digitIndex] = singleDigits[digitIndex] || []
+    singleDigits[digitIndex].push(line)
+  })
+  return singleDigits
+}
+const rawDigits = `
+╔══╗
+║  ║
+╚══╝
+   ╗
+   ║
+   ╝
+╔══╗
+╔══╝
+╚══╝
+ ══╗
+  ═╣
+ ══╝
+╔  ╗
+╚══╣
+   ╝
+╔══╗
+╚══╗
+╚══╝
+╔══╗
+╠══╗
+╚══╝
+╔══╗
+   ║
+   ╝
+╔══╗
+╠══╣
+╚══╝
+╔══╗
+╚══╣
+╚══╝
+`
