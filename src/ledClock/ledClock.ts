20b10ca45b701b7918c3805d9a14850fe30ddbed
Lukas Elmer <lukas.elmer@gmail.com>
2023-12-03 11:34:38 +0100

    Scripted auto-commit on change (2023-12-03 11:34:37) by gitwatch.sh

--- src/ledClock/ledClock.ts
+++ src/ledClock/ledClock.ts
@@ -1,18 +1,18 @@
 import { consoleLog } from '../logger'
 
 export function ledClock(value: number) {
-  if (value === 8) {
-    consoleLog('')
-    consoleLog('╔══╗')
-    consoleLog('╠══╣')
-    consoleLog('╚══╝')
-    consoleLog('')
-  }
   if (value === 3) {
     consoleLog('')
     consoleLog(' ══╗')
     consoleLog('  ═╣')
     consoleLog(' ══╝')
+    consoleLog('')
+  }
+  if (value === 8) {
+    consoleLog('')
+    consoleLog('╔══╗')
+    consoleLog('╠══╣')
+    consoleLog('╚══╝')
     consoleLog('')
   }
 }
