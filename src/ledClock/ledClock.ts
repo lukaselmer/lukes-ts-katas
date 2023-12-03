a1b33feeed9e89bcd90a5c0ba4d03689d33e7616
Lukas Elmer <lukas.elmer@gmail.com>
2023-12-03 12:55:48 +0100

    Scripted auto-commit on change (2023-12-03 12:55:48) by gitwatch.sh

--- src/ledClock/ledClock.ts
+++ src/ledClock/ledClock.ts
@@ -2,6 +2,11 @@ import { consoleLog } from '../logger'
 import { singleDigit } from './singleDigit'
 
 export function ledClock(value: number) {
+  const digits = value
+    .toString()
+    .split('')
+    .map((digit) => parseInt(digit, 10))
+  console.log(digits)
   consoleLog('')
   singleDigit(value).forEach((line) => consoleLog(line))
   consoleLog('')
